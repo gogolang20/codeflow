e6c7e4d4313ed15aeadee18e0ca7ea5ccb593b9c
Mike Sun <not yet>
2023-11-08 20:19:50 +0800

    first commit

--- web_gin/lesson38-Login/serializer/common.go
+++
@@ -1,8 +0,0 @@
-package serializer
-
-type Response struct {
-	Code  int         `json:"code"`
-	Data  interface{} `json:"data"`
-	Msg   interface{} `json:"msg"`
-	Error string      `json:"error"`
-}
